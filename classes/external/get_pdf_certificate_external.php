3faf56c8478b2ae59504e8e71b942a32d908d6b1
Elena <elena@tresipunt.com>
2024-06-13 12:25:33 +0200

    UNIMOODP31-10: external web service mod_certifygen_get_id_instance_certificate_external added

--- classes/external/get_pdf_certificate_external.php
+++ classes/external/get_pdf_certificate_external.php
@@ -36,7 +36,12 @@ use external_api;
 use external_function_parameters;
 use external_single_structure;
 use external_value;
+use mod_certifygen\persistents\certifygen;
+use mod_certifygen\persistents\certifygen_model;
+use mod_certifygen\persistents\certifygen_validations;
 
+global $CFG;
+require_once($CFG->dirroot.'/user/lib.php');
 class get_pdf_certificate_external extends external_api {
     /**
      * Describes the external function parameters.
@@ -60,7 +65,75 @@ class get_pdf_certificate_external extends external_api {
          * del contexto de generación del pdf. No se realizará ninguna comprobación como podría ser si el customfield
          * es “readonly” o no.
         */
-        return ['file' => 'file'];
+        // PENDIENTE:
+        // TODO:customfields... que modifica el fichero.
+        $params = self::validate_parameters(
+            self::get_pdf_certificate_parameters(), ['userid' => $userid, 'idinstance' => $idinstance, 'customfields' => $customfields]
+        );
+        $result = ['file' => '', 'error' => []];
+        $haserror = false;
+        try {
+            // User exists.
+            $user = user_get_users_by_id([$params['userid']]);
+            if (empty($user)) {
+                unset($result['file']);
+                $result['error']['code'] = 'user_not_found';
+                $result['error']['message'] = 'User not found';
+                return $result;
+            }
+            // Activity exists?
+            $certifygen = new certifygen($params['idinstance']);
+
+            // Is user enrolled on this course as student?
+            $mycourses = enrol_get_users_courses($params['userid'], true, 'id');
+            if (!in_array($certifygen->get('course'), array_keys($mycourses))) {
+                unset($result['file']);
+                $result['error']['code'] = 'user_not_enrolled_on_idinstance_course';
+                $result['error']['message'] = 'User not enrolled on idinstance course';
+                return $result;
+            }
+
+            // Model info.
+            $model = new certifygen_model($certifygen->get('modelid'));
+            if (is_null($model->get('validation'))) {
+                unset($result['file']);
+                $result['error']['code'] = 'model_has_no_validation';
+                $result['error']['message'] = 'This model is configured with no validation.';
+                return $result;
+            }
+
+            // Process status
+            // TODO: me tendrian  especificar el idioma??.
+            $validations = certifygen_validations::get_records(['userid' => $params['userid'], 'modelid' => $model->get('id')]);
+            foreach ($validations as $validation) {
+                if ($validation->get('status') != certifygen_validations::STATUS_IN_PROGRESS) {
+                    continue;
+                }
+                $file = \mod_certifygen\certifygen::get_user_certificate_file($model->get('templateid'), $userid,
+                    $certifygen->get('course'), $validation->get('lang'));
+                if (is_null($file)) {
+                    $haserror = true;
+                    $result['error']['code'] = 'file_not_found';
+                    $result['error']['message'] = 'File not found';
+                } else {
+                    $result['file'] = $file->get_contenthash(); // PARAM_FILE
+                }
+            }
+        } catch (\moodle_exception $e) {
+            error_log("error: ".var_export($e, true));
+            unset($result['file']);
+            $haserror = true;
+            $result['error']['code'] = $e->errorcode;
+            $result['error']['message'] = $e->getMessage();
+        }
+
+        if (!$haserror) {
+            unset($result['error']);
+        } else {
+            unset($result['file']);
+        }
+
+        return $result;
     }
     /**
      * Describes the data returned from the external function.
@@ -68,9 +141,13 @@ class get_pdf_certificate_external extends external_api {
      * @return external_single_structure
      */
     public static function get_pdf_certificate_returns(): external_single_structure {
-        return new external_single_structure(array(
-                'file' => new external_value(PARAM_RAW, 'Certificate file'),
-            )
+        return new external_single_structure([
+                'file' => new external_value(PARAM_RAW, 'Certificate file', VALUE_OPTIONAL),
+                'error' => new external_single_structure([
+                    'message' => new external_value(PARAM_CLEANFILE, 'Error message'),
+                    'code' => new external_value(PARAM_RAW, 'Error code'),
+                ], 'Errors information', VALUE_OPTIONAL),
+            ]
         );
     }
 }
