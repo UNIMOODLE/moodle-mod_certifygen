612259e80bb6b7d78709f2730b4d7951b77e35b7
Elena <elena@tresipunt.com>
2024-06-13 12:49:16 +0200

    UNIMOODP31-10: external web service mod_certifygen_get_pdf_certificate_external added

--- classes/external/get_json_certificate_external.php
+++ classes/external/get_json_certificate_external.php
@@ -36,7 +36,12 @@ use external_api;
 use external_function_parameters;
 use external_single_structure;
 use external_value;
+use mod_certifygen\persistents\certifygen;
+use mod_certifygen\persistents\certifygen_model;
+use mod_certifygen\persistents\certifygen_validations;
 
+global $CFG;
+require_once($CFG->dirroot.'/user/lib.php');
 class get_json_certificate_external extends external_api {
     /**
      * Describes the external function parameters.
@@ -59,7 +64,75 @@ class get_json_certificate_external extends external_api {
          * obtención de los datos del proceso de generación del documento con la presentación
          * final.
          */
-        return ['json' => '{"elemento1":"imagen"}'];
+        // TODO:customfields... que modifica el fichero.
+        $params = self::validate_parameters(
+            self::get_json_certificate_parameters(), ['userid' => $userid, 'idinstance' => $idinstance, 'customfields' => $customfields]
+        );
+        $result = ['json' => '', 'error' => []];
+        $haserror = false;
+        try {
+            // User exists.
+            $user = user_get_users_by_id([$params['userid']]);
+            if (empty($user)) {
+                unset($result['json']);
+                $result['error']['code'] = 'user_not_found';
+                $result['error']['message'] = 'User not found';
+                return $result;
+            }
+            // Activity exists?
+            $certifygen = new certifygen($params['idinstance']);
+
+            // Is user enrolled on this course as student?
+            $mycourses = enrol_get_users_courses($params['userid'], true, 'id');
+            if (!in_array($certifygen->get('course'), array_keys($mycourses))) {
+                unset($result['json']);
+                $result['error']['code'] = 'user_not_enrolled_on_idinstance_course';
+                $result['error']['message'] = 'User not enrolled on idinstance course';
+                return $result;
+            }
+
+            // Model info.
+            $model = new certifygen_model($certifygen->get('modelid'));
+            if (is_null($model->get('validation'))) {
+                unset($result['json']);
+                $result['error']['code'] = 'model_has_no_validation';
+                $result['error']['message'] = 'This model is configured with no validation.';
+                return $result;
+            }
+
+            // Process status
+            // TODO: me tendrian  especificar el idioma??.
+            $validations = certifygen_validations::get_records(['userid' => $params['userid'], 'modelid' => $model->get('id')]);
+            foreach ($validations as $validation) {
+                if ($validation->get('status') != certifygen_validations::STATUS_IN_PROGRESS) {
+                    continue;
+                }
+                $issue = \mod_certifygen\certifygen::get_user_certificate($params['userid'], $certifygen->get('course'),
+                    $model->get('templateid'), $validation->get('lang'));
+
+                if (is_null($issue)) {
+                    $haserror = true;
+                    $result['error']['code'] = 'issue_not_found';
+                    $result['error']['message'] = 'Issue not found';
+                } else {
+                    $result['json'] = $issue->data;
+                }
+            }
+        } catch (\moodle_exception $e) {
+            error_log("error: ".var_export($e, true));
+            unset($result['json']);
+            $haserror = true;
+            $result['error']['code'] = $e->errorcode;
+            $result['error']['message'] = $e->getMessage();
+        }
+
+        if (!$haserror) {
+            unset($result['error']);
+        } else {
+            unset($result['json']);
+        }
+
+        return $result;
     }
     /**
      * Describes the data returned from the external function.
@@ -67,9 +140,13 @@ class get_json_certificate_external extends external_api {
      * @return external_single_structure
      */
     public static function get_json_certificate_returns(): external_single_structure {
-        return new external_single_structure(array(
-                'json' => new external_value(PARAM_RAW, 'Certificate elements in a json'),
-            )
+        return new external_single_structure([
+                'json' => new external_value(PARAM_RAW, 'Certificate elements in a json', VALUE_OPTIONAL),
+                'error' => new external_single_structure([
+                    'message' => new external_value(PARAM_CLEANFILE, 'Error message'),
+                    'code' => new external_value(PARAM_RAW, 'Error code'),
+                ], 'Errors information', VALUE_OPTIONAL),
+            ],
         );
     }
 }
