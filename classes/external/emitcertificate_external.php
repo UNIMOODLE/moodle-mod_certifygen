8847f2274be59f2cb2f78c83badd96a69362ab82
elena <elena@tresipunt.com>
2024-08-13 17:58:30 +0200

    UNIMOODP31-10: cmd validation - after emiting a certificate, change status to finish

--- classes/external/emitcertificate_external.php
+++ classes/external/emitcertificate_external.php
@@ -151,7 +151,7 @@ class emitcertificate_external extends external_api {
                 $validationplugin = $certifygenmodel->get('validation');
                 $validationpluginclass = $validationplugin . '\\' . $validationplugin;
                 if (empty($validationplugin)) {
-                    $validation->set('status', certifygen_validations::STATUS_VALIDATION_OK);
+                    $validation->set('status', certifygen_validations::STATUS_FINISHED);
                     $validation->save();
                 } else if (get_config($validationplugin, 'enabled') === '1') {
                     /** @var ICertificateValidation $subplugin */
