78d95558520403b52efc983ecb2b3e58a47733ad
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2025-01-31 18:20:29 +0000

    Fix change of core_external namespace

--- classes/external/getteacherrequestviewdata_external.php
+++ classes/external/getteacherrequestviewdata_external.php
@@ -35,12 +35,12 @@ namespace mod_certifygen\external;
 use coding_exception;
 use context_system;
 use dml_exception;
-use external_api;
+use core_external\external_api;
 use invalid_parameter_exception;
 use mod_certifygen\output\views\profile_my_certificates_view;
-use external_function_parameters;
-use external_single_structure;
-use external_value;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
 use moodle_exception;
 
 /**
