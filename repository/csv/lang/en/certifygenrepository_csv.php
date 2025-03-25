b775a1ca9a849ca80eff31a4aa09bd5a0023a961
Franco Pantoja <francisco.f@idef21.com>
2025-03-25 18:02:58 +0100

    Add missing strings #103

--- repository/csv/lang/en/certifygenrepository_csv.php
+++ repository/csv/lang/en/certifygenrepository_csv.php
@@ -35,3 +35,4 @@ $string['pluginnamesettings'] = 'CSV Repository Settings';
 $string['enable'] = 'Enable';
 $string['enable_help'] = 'This repository can only be used woth csv validation plugin.';
 $string['privacy:metadata'] = 'The CSV Repository plugin does not store any personal data.';
+$string['certifygenvalidationcsvnotfound'] = 'CSV Validation plugin not found';
