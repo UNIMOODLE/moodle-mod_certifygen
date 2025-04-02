969734c40089a1fa1425ce55e76676227b006a9a
Franco Pantoja <francisco.f@idef21.com>
2025-04-02 11:49:19 +0200

    Add teacher selector by capability #107

--- classes/persistents/certifygen_context.php
+++ classes/persistents/certifygen_context.php
@@ -148,23 +148,9 @@ class certifygen_context extends persistent {
      * @throws dml_exception
      */
     public static function can_i_see_teacherrequestlink(int $userid): bool {
-        global $DB;
-        $comparename = $DB->sql_compare_text('r.shortname');
-        $comparenameplaceholder = $DB->sql_compare_text(':shortname');
-        $select = "AND  {$comparename} = {$comparenameplaceholder}";
-
-        $sql = "SELECT COUNT(c.id) as num
-                  FROM {course} c
-                  JOIN {enrol} e ON e.courseid = c.id
-                  JOIN {user_enrolments} ue ON ue.enrolid = e.id
-                  JOIN {user} u ON ue.userid = u.id
-                  JOIN {role_assignments} ra ON ra.userid = u.id
-                  JOIN {context} con ON ( con.id = ra.contextid AND con.contextlevel = 50 AND con.instanceid = c.id)
-                  JOIN {role} r ON r.id = ra.roleid
-                 WHERE u.id = :userid $select";
-        $result = $DB->get_records_sql($sql, ['userid' => $userid, 'shortname' => 'editingteacher']);
-        $result = reset($result);
-        if ($result->num > 0) {
+        $result = get_user_capability_course('mod/certifygen:addinstance', $userid);
+        $numresult = count($result);
+        if ($numresult > 0) {
             return true;
         }
         return false;
