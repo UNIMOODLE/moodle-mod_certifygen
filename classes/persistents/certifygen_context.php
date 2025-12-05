fda4728ad3a455f19f41b0845a03e6c879e594fe
Juan Pablo de Castro Fernández <juan.pablo.de.castro@gmail.com>
2025-12-05 14:41:46 +0100

    Fix usage of array|bool return type.
    Function returns false if no users found.

--- classes/persistents/certifygen_context.php
+++ classes/persistents/certifygen_context.php
@@ -149,8 +149,7 @@ class certifygen_context extends persistent {
      */
     public static function can_i_see_teacherrequestlink(int $userid): bool {
         $result = get_user_capability_course('mod/certifygen:addinstance', $userid);
-        $numresult = (is_countable ($result)) ? count($result) : 0;
-        if ($numresult > 0) {
+        if ($result && count($result) > 0) {
             return true;
         }
         return false;
