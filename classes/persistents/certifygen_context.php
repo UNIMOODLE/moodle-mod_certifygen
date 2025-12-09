b0f4d68f110e1d557e0b35f61c7210ada15adf0d
elena <elena@tresipunt.com>
2025-12-09 13:51:57 +0100

    issue 120: manage non countable get_user_capability_course function result

--- classes/persistents/certifygen_context.php
+++ classes/persistents/certifygen_context.php
@@ -149,6 +149,9 @@ class certifygen_context extends persistent {
      */
     public static function can_i_see_teacherrequestlink(int $userid): bool {
         $result = get_user_capability_course('mod/certifygen:addinstance', $userid);
+        if (!$result) {
+            return false;
+        }
         $numresult = count($result);
         if ($numresult > 0) {
             return true;
